74e5be6ca90e188934ee521377fffe549b54411b
SHANMUR1988 <147174327+SHANMUR1988@users.noreply.github.com>
2023-10-06 22:25:45 +0530

    Add files via upload

---
+++ Functions in string data type.py
@@ -0,0 +1,34 @@
+# Below are the String functions in python
+"""
+string.ascii_uppercase
+string.digits
+string.hexdigits
+string.octdigits
+string.punctuation
+string.printable
+string.whitespace
+format(format_string, /, *args, **kwargs)
+vformat(format_string, args, kwargs)
+parse(format_string)
+get_field(field_name, args, kwargs)
+get_value(key, args, kwargs)
+check_unused_args(used_args, args, kwargs)
+format_field(value, format_spec)
+convert_field(value, conversion)
+replacement_field
+field_name
+arg_name
+attribute_name
+element_index
+index_string
+conversion
+format_spec
+format_spec
+fill
+align
+sign
+width
+grouping_option
+precision
+type
+"""
