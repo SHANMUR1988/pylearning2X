3d704ea8a264feb3aaa3c83c7e60fae101c42e93
SHANMUR1988 <147174327+SHANMUR1988@users.noreply.github.com>
2023-10-06 20:11:23 +0530

    Add files via upload

---
+++ Lab 003.py
@@ -0,0 +1,12 @@
+#1
+name = input ("Enter your name")
+print(name)
+
+#2
+name1 = input ("Enter your First Name")
+name2 = input ("Enter your Last Name")
+print(name1, name2, sep="-", end= "\t""\n")
+
+#3
+output = input ("Enter your name")
+print(f"You are welcome {output} to the Python Class")
